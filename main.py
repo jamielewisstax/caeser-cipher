67353f100d24e08121d661630cd800832355b470
Jamie Lewis Stax <107413598+jamielewisstax@users.noreply.github.com>
2022-09-14 22:10:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+
+def caeser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        if letter not in alphabet:
+            end_text += letter
+        else:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            if new_position <= 0:
+                new_position = new_position % len(alphabet) + position
+            if new_position >= len(alphabet):
+                new_position = new_position % len(alphabet)
+            end_text += alphabet[new_position]
+    print(f"Here's the {direction}d result: {end_text}\n")
+
+
+print(logo)
+restart = True
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
+    while True:
+        try_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+        if try_again not in ("yes", "no"):
+            print("Please enter only 'yes' or 'no'.")
+            continue
+        elif try_again == "no":
+            restart = False
+            print("Goodbye")
+            break
+        elif try_again == "yes":
+            break
